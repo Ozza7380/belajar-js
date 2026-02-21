4c602f07512e2622301e67070559029b3c75441b
M Raihan H <ar937464@gmail.com>
2026-02-21 11:51:27 +0700

    repeat

---
+++ 09-string(level2)/repeat.js
@@ -0,0 +1,8 @@
+// mengulang teks
+console.log("*".repeat(5))
+
+// buat pw sensor
+let pw = "rahasia123"
+// tampilkan kode menjadi: ********
+let sensor = "*".repeat(pw.length)
+console.log(sensor)
