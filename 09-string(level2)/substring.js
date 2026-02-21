9238c18ed537c9f85ee69637476e77f0f2a79d08
M Raihan H <ar937464@gmail.com>
2026-02-21 11:47:18 +0700

    substring

---
+++ 09-string(level2)/substring.js
@@ -0,0 +1,8 @@
+// mirip slice() untuk ambil sebagian teks
+let kode = "INV2025001"
+console.log(kode.substring(3,7)) // memotong dari index 3-6
+// ambil tahun dari kode
+let code = "TRX20241234"
+// target: 2024
+let tahun = kode.substring(3,7);
+console.log(tahun)
