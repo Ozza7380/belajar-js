a654aa7ed07ee90a5bf0f5490af05003821e53f9
M Raihan H <ar937464@gmail.com>
2026-02-21 12:01:51 +0700

    charAt

---
+++ 09-string(level2)/charAt.js
@@ -0,0 +1,9 @@
+let name = "Muhammad Raihan"
+console.log(name.charAt(0))
+
+// ambil nilai inisial
+let nama = "Muhammad Raihan"
+// target: MR
+let kata = nama.split(" ");
+let inisial = kata[0].charAt(0) + kata[1].charAt(0)
+console.log(inisial)
---
+++ 09-string(level2)/padStart-padEnd.js
@@ -0,0 +1,8 @@
+let angka = "15"
+console.log(angka.padStart(3,"0")) // jika angka tidak lebih dari 3 maka diawal akan di tambah 0
+
+// no antri
+let nomor = "7"
+//target: 007
+let hasil = nomor.padStart(3,"0")
+console.log(hasil)
