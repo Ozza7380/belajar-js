7f816ecb01c67605b84277128e01a453d849138b
M Raihan H <ar937464@gmail.com>
2026-02-21 10:34:11 +0700

    ambil username dari email

---
+++ 08-string/ambilUsernameDariEmail.js
@@ -0,0 +1,4 @@
+let email = "ariska@gmail.com"
+
+let username = email.split("@")[0];// memisahkan email menjadi array dengan pemisahnya @, dan index yang di tampilkan [0] (indek 0)
+console.log(username)
