f401d09bf35c993124923a943366968be3daf03c
M Raihan H <ar937464@gmail.com>
2026-02-21 10:41:55 +0700

    validasi panjang pw

--- 08-string/sensorKata.js
+++ 08-string/sensorKata.js
@@ -2,5 +2,5 @@ let komentar = "produk ini jelek banget";
 
 // mengganti kata jelek menjadi '****'
 
-let hasil = komentar.replace("jelek", "****") // replace("", ""): untuk mengganti kata 
-console.log(hasil)
+let hasil = komentar.replace("jelek", "****"); // replace("", ""): untuk mengganti kata 
+console.log(hasil);
---
+++ 08-string/validasiPanjangPassword.js
@@ -0,0 +1,7 @@
+let password = "abc123"
+
+// jika kurang dari 8 karakter tampilkan eror
+
+if(password.length < 8) {
+    console.log("password terlalu pendek");
+}
