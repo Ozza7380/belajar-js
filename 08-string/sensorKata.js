0f18a91903143a8f3563635ff7b3df04756f8403
M Raihan H <ar937464@gmail.com>
2026-02-21 10:37:43 +0700

    sensor kata

---
+++ 08-string/sensorKata.js
@@ -0,0 +1,6 @@
+let komentar = "produk ini jelek banget";
+
+// mengganti kata jelek menjadi '****'
+
+let hasil = komentar.replace("jelek", "****") // replace("", ""): untuk mengganti kata 
+console.log(hasil)
