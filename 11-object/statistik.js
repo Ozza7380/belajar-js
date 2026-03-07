2c0658dcca373531770513a21fa00096598df263
M Raihan H <ar937464@gmail.com>
2026-03-07 11:19:00 +0700

    statistik (object)

---
+++ 11-object/statistik.js
@@ -0,0 +1,16 @@
+const statistik = {
+    totalUser: 120,
+    totalTransaksi: 80,
+    totalPendapatan: 15000000
+};
+statistik.totalProduk = 50
+
+console.log(statistik.totalProduk)
+
+let rataRataTransaksi = statistik.totalPendapatan / statistik.totalTransaksi
+
+console.log(rataRataTransaksi)
+
+statistik.totalPendapatan = 20000000
+
+console.log(statistik.totalPendapatan)
