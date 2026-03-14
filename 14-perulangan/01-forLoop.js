4509b26632f84fada5dc3c10dd812d8f7a43acb7
M Raihan H <ar937464@gmail.com>
2026-03-14 11:18:42 +0700

    perulangan (for loop)

---
+++ 14-perulangan/01-forLoop.js
@@ -0,0 +1,7 @@
+// 1. for loop 
+//(struktur dasar) for (inisialisasi; kondisi; increment) { kode yang diulang}
+
+// cetak "perulanagan ke-" sampai 5 kali
+for (let i = 1; i <= 5; i++) { // for = perulangan, let i = 1 (membuat variable baru), i lebih kecil sama dengan 5, i++ = perulangan 1x (jika sudah menjalankan "perulangan ke-" samapai lima kali maka akan berhenti), i+2 perulangan 2x (jika sudah menjalankan "perulangan ke-" samapai lima kali maka akan mengulangi nya sekali lagi)
+    console.log("perulangan ke-" + i) // 
+}
