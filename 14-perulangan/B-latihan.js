839d2eb010e7db24ebca728f6419c45e733a91cd
M Raihan H <ar937464@gmail.com>
2026-03-14 11:31:23 +0700

    perulangan (latihan 2)

---
+++ 14-perulangan/B-latihan.js
@@ -0,0 +1,6 @@
+// B. latihan2 (hitung jumlah angka dari 1-5)
+let total = 0
+for (let i = 1; i <= 100; i++) {
+    total += i // "total" ditambah "i" (nilai i akan terus membesar sampai perulangan habis)
+}
+console.log(total)
