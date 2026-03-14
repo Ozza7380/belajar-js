07b50835293862fde72c548186bd4779a196af0e
M Raihan H <ar937464@gmail.com>
2026-03-14 10:47:16 +0700

    percabangan (koversi nilai)

---
+++ 13-percabangann/penilaian.js
@@ -0,0 +1,14 @@
+// 3. penilaian (koversi nilai ke huruf)
+let nilai = 75
+
+if (nilai >= 90) {
+    console.log("A") // jika nilai lebih atau sama dengan 90 maka nilai "A"
+} else if (nilai >= 80) {
+    console.log("B") // jika nilai lebih atau sama dengan 80 maka nilai "B"
+} else if (nilai >= 70) {
+    console.log("C") // jika nilai lebih atau sama dengan 70 maka nilai "C"
+} else if (nilai >= 60) {
+    console.log("D") // jika nilai lebih atau sama dengan 60 maka nilai "D"
+} else {
+    console.log("E")// jika nilai lebih atau sama dengan 50 maka nilai "E"
+}
