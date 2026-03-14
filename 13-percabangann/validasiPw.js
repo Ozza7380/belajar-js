6df19c6defb9163b2951b9080ba5446432d27f0f
M Raihan H <ar937464@gmail.com>
2026-03-14 10:46:11 +0700

    percabangan (validasi login)

---
+++ 13-percabangann/validasiPw.js
@@ -0,0 +1,9 @@
+// 2. Validasi login (simulasi login sederhana)
+let username = "admin"
+let password = "1234"
+// jika username = "admin" dan password adalah "1234" maka "login berhasil", jika tidak "ussername atau password salah"
+if (username === "admin" && password === "1234") {
+    console.log("login berhasil")
+} else {
+    console.log("username atau password salah")
+}
