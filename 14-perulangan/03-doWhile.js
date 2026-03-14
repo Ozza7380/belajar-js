d997c97e6803a6d354b0e119932785bb56037592
M Raihan H <ar937464@gmail.com>
2026-03-14 11:51:30 +0700

    perulangan (do while)

---
+++ 14-perulangan/03-doWhile.js
@@ -0,0 +1,8 @@
+// 3. do while (perulangan minimal jalan sekali)
+// struktur = do { // kode } while (kondisi)
+
+let i = 1
+do {
+    console.log(i)
+    i++         // lakukan perulangan
+} while (i <= 5)// batasan perulangan
