94e4dc60fae948dc879500de71491baee0a4163d
M Raihan H <ar937464@gmail.com>
2026-03-14 11:37:07 +0700

    perulangan (while)

--- 14-perulangan/01-forLoop.js
+++ 14-perulangan/01-forLoop.js
@@ -1,5 +1,5 @@
 // 1. for loop 
-//(struktur dasar) for (inisialisasi; kondisi; increment) { kode yang diulang}
+//struktur= for (inisialisasi; kondisi; increment) { kode yang diulang}
 
 // cetak "perulanagan ke-" sampai 5 kali
 for (let i = 1; i <= 5; i++) { // for = perulangan, let i = 1 (membuat variable baru), i lebih kecil sama dengan 5, i++ = perulangan 1x (jika sudah menjalankan "perulangan ke-" samapai lima kali maka akan berhenti), i+2 perulangan 2x (jika sudah menjalankan "perulangan ke-" samapai lima kali maka akan mengulangi nya sekali lagi)
---
+++ 14-perulangan/02-whileLoop.js
@@ -0,0 +1,8 @@
+// 2.  while loop (while akan terus berjalan selama kondisi bernilai true)
+// struktur = while (kondisi) { // kode }
+let i = 1
+
+while (i <= 5) {
+    console.log(i)
+    i++ // i++ agar while berhenti
+}
