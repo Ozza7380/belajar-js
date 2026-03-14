31081d715be19e3d7a894838c602ae568ccf1ff8
M Raihan H <ar937464@gmail.com>
2026-03-14 11:46:09 +0700

    perulangan (latihan 3)

---
+++ 14-perulangan/C-latihan.js
@@ -0,0 +1,8 @@
+// C. latihan3 (cetak angka mundur dari 10 ke 1)
+let i = 100
+while (i >= 60) {
+    if (i % 2 == 0) {// untuk menamppilkang angka genap
+        console.log(i)
+    }
+    i-- // i = i -1 (angka i akan berkurang samapai angka yang ditentukan)
+}
