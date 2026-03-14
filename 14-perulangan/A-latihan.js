48b272e69797bab9120427042e24206243f98ebf
M Raihan H <ar937464@gmail.com>
2026-03-14 11:25:47 +0700

    perulangan (latihan 1)

---
+++ 14-perulangan/A-latihan.js
@@ -0,0 +1,13 @@
+// A. latihan mencetak angka genap dari 2 sampai 20
+for (let i = 2; i <= 20; i += 2) { // i ditambah 2 sampai nilai i 20 (false)
+/*
+i += 2 
+sama dengan 
+i = i + 2
+
+i++
+sama dengan
+i = i + 1
+*/ 
+console.log(i)
+} 
