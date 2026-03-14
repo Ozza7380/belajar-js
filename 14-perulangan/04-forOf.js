161486962ca2b5a56ee895e8e0e45a131f54f028
M Raihan H <ar937464@gmail.com>
2026-03-14 11:56:38 +0700

    perulangan (for of)

---
+++ 14-perulangan/04-forOf.js
@@ -0,0 +1,6 @@
+// 4. for of (loop untuk array) (digunakan untuk membaca isi array)
+let buah = ["apel", "jeruk", "mangga"]
+
+for (let item of buah) { // let item of buah (untuk memisahkan array)
+    console.log(item)
+}
