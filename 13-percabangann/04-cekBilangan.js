a58078bf021c34bdfe7e178406836d6db3e44145
M Raihan H <ar937464@gmail.com>
2026-03-14 10:53:56 +0700

    percabangan (cek bilangan)

---
+++ 13-percabangann/04-cekBilangan.js
@@ -0,0 +1,10 @@
+// 4. cek bilangan
+let angka = -10
+
+if (angka > 0) {
+    console.log("positif") // jika angka kurang dari 0 maka positif
+} else if (angka < 0) {
+    console.log("negatif") // jika angka lebih dari 0 maka negatif
+} else {
+    console.log("nol") // jika lainnya maka nol
+}
