5c50be171598ad643fea40e01b190bd5ede9b1b3
M Raihan H <ar937464@gmail.com>
2026-03-14 10:45:52 +0700

    percabangan (validasi umur)

---
+++ 13-percabangann/percabangan.js
@@ -0,0 +1,8 @@
+// 1. Validasi umur
+let umur = 16
+// (if) jika umur lebih dari atau sama dengan 17 maka boleh membuat sim, (else) jika tidak maka tidak boleh
+if (umur >= 17) {
+    console.log("anda boleh membuat sim")
+} else {
+    console.log("anda belum cukup umur")
+}
